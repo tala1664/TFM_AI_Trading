e848c650eed30fe992d368ca22f952ee6770bd8f
Antonio <antoniodlm96@gmail.com>
2022-09-25 14:42:58 +0200

    Fixes and download more data

--- lab/yahoo_finance_test.py
+++ lab/yahoo_finance_test.py
@@ -4,26 +4,29 @@ from pyspark.sql import SparkSession
 
 # https://pypi.org/project/yfinance/
 
-def download_market_data(stock, period, interval):
-    spark = SparkSession.builder \
-        .master("local[1]") \
-        .appName("SparkByExamples.com") \
-        .getOrCreate()
 
-    data = yf.download(stock, period="max", interval="1m")
+def download_market_data(spark, stock, period, interval):
+    data = yf.download(stock, period=period, interval=interval)
 
     data['DateTime'] = data.index
 
-    df = spark.createDataFrame(data)
-    return df
+    dataframe = spark.createDataFrame(data)
+    return dataframe
 
 
-def write_market_data(stock, period, interval):
-    df.write.format("parquet").mode("overwrite").save("../data/" + stock +
-                                                      "_period=" + period +
-                                                      "_interval=" + interval +
-                                                      ".parquet")
+def write_market_data(dataframe, stock, period, interval):
+    dataframe.write.format("parquet").mode("overwrite").save("../data/" + stock +
+                                                             "_period=" + period +
+                                                             "_interval=" + interval +
+                                                             ".parquet")
 
 
-df = download_market_data("GOOGL", "max", "1d")
-write_market_data("GOOGL", "max", "1d")
+spk = SparkSession.builder \
+    .master("local[1]") \
+    .appName("SparkByExamples.com") \
+    .getOrCreate()
+
+stk_array = ["AAPL", "GOOGL", "SSNLF", "MSFT", "TSLA", "DELL", "META"]
+for stk in stk_array:
+    df = download_market_data(spk, stk, "max", "1d")
+    write_market_data(df, stk, "max", "1d")
