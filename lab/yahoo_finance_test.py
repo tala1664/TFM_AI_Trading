0d78d60a7ad1d1cf8d9b8d760ad1d5f7dbdaa3a0
Antonio <antoniodlm96@gmail.com>
2022-09-25 14:12:14 +0200

    Download data from yahoo into parquet

--- lab/yahoo_finance_test.py
+++ lab/yahoo_finance_test.py
@@ -1,15 +1,18 @@
-import pandas
 import yfinance as yf
 from pyspark.sql import SparkSession
+
+stock = "GOOGL"
 
 spark = SparkSession.builder \
     .master("local[1]") \
     .appName("SparkByExamples.com") \
     .getOrCreate()
 
-data = yf.download("SPY AAPL", period="1d", interval="1m")
-print(data)
-df = pandas.DataFrame(data=data)
+data = yf.download(stock, period="1d", interval="1m")
 
-df2 = spark.createDataFrame(df)
-df2.show(10, False)
+data['DateTime'] = data.index
+
+df = spark.createDataFrame(data)
+df.show(10, False)
+
+df.write.format("parquet").save("../data/" + stock + ".parquet")
