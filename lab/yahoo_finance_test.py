62ca487028ba2f266524db5353787628e9873560
Antonio <antoniodlm96@gmail.com>
2022-09-25 14:31:41 +0200

    Structure in functions

--- lab/yahoo_finance_test.py
+++ lab/yahoo_finance_test.py
@@ -1,18 +1,29 @@
 import yfinance as yf
 from pyspark.sql import SparkSession
 
-stock = "AAPL"
 
-spark = SparkSession.builder \
-    .master("local[1]") \
-    .appName("SparkByExamples.com") \
-    .getOrCreate()
+# https://pypi.org/project/yfinance/
 
-data = yf.download(stock, period="1d", interval="1m")
+def download_market_data(stock, period, interval):
+    spark = SparkSession.builder \
+        .master("local[1]") \
+        .appName("SparkByExamples.com") \
+        .getOrCreate()
 
-data['DateTime'] = data.index
+    data = yf.download(stock, period="max", interval="1m")
 
-df = spark.createDataFrame(data)
-df.show(10, False)
+    data['DateTime'] = data.index
 
-df.write.format("parquet").save("../data/" + stock + ".parquet")
+    df = spark.createDataFrame(data)
+    return df
+
+
+def write_market_data(stock, period, interval):
+    df.write.format("parquet").mode("overwrite").save("../data/" + stock +
+                                                      "_period=" + period +
+                                                      "_interval=" + interval +
+                                                      ".parquet")
+
+
+df = download_market_data("GOOGL", "max", "1d")
+write_market_data("GOOGL", "max", "1d")
