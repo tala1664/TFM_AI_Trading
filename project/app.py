519a3f57b85dc8b221bf2f2ebd6159a8784cc777
Antonio <antoniodlm96@gmail.com>
2023-01-07 13:29:34 +0100

    Refactor and adjustments

--- project/app.py
+++ project/app.py
@@ -1,11 +1,10 @@
 import os
 import sys
 from io_stockdata.io_stockdata import download_stock_data, write_stock_data, \
-    read_stock_data, read_stock_log, read_portfolio_list, write_portfolio_list
-from finance.finance_utils import correlation_matrix_portfolio
+    read_stock_data, read_stock_log, read_portfolio_list
+from finance.finance_utils import correlation_matrix_portfolio, create_portfolio
 from display.display_utils import display_graph
 from pyspark.sql import SparkSession
-import datetime
 
 
 def get_valid_period():
@@ -51,22 +50,23 @@ def main():
         option = int(input("Select a valid option: \n" +
                            "0. Exit\n" +
                            "1. Download stock data.\n" +
-                           "2. Show stock graph. \n" +
-                           "3. Update ALL. \n" +
-                           "4. Create Portfolio. \n" +
-                           "5. Show Portfolio Table. \n" +
-                           "6. Show Stock Table. \n" +
+                           "2. Show Stock graph. \n" +
+                           "3. Show Stock Table. \n" +
+                           "4. Update ALL Stocks. \n" +
+                           "5. Create Portfolio. \n" +
+                           "6. Show Portfolio Table. \n" +
                            "7. Calculate portfolio correlation matrix. \n"))
 
         if option == 1:
+            stk = input("Please, input a valid stock name: ").upper()
+
             try:
-                stk = input("Please, input a valid stock name: ").upper()
                 period = get_valid_period()
                 interval = get_valid_interval()
                 df = download_stock_data(spark, stk, period, interval)
                 write_stock_data(spark, df, stk, period, interval)
 
-            except:
+            except ValueError:
                 print("Symbol " + stk + " not found")
 
         elif option == 2:
@@ -77,57 +77,10 @@ def main():
                 interval = get_valid_interval()
                 df = read_stock_data(spark, stk, period, interval)
                 display_graph(df, "DateTime", "Close", stk)
-            except:
+            except (TypeError, AttributeError):
                 print("Empty data, try option -> 1. Download stock data.\n")
 
         elif option == 3:
-            df_stock = read_stock_log(spark)
-            df_stock.orderBy("Stock").orderBy("Stock").show(truncate=False)
-            list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
-            list_period = df_stock.select("Period").rdd.flatMap(lambda x: x).collect()
-            list_interval = df_stock.select("Interval").rdd.flatMap(lambda x: x).collect()
-            for i in range(len(list_stock)):
-                df = download_stock_data(spark, list_stock[i], list_period[i], list_interval[i])
-                write_stock_data(spark, df, list_stock[i], list_period[i], list_interval[i])
-
-        elif option == 4:
-            df_stock = read_stock_log(spark)
-            df_stock.orderBy("Stock").filter("Period = 'max'").orderBy("Stock").show(truncate=False)
-            list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
-            stk_list = input("Please, input a list separated by comma of stocks: ").upper().replace(" ", "").split(",")
-            stk_list = [i for i in stk_list if i in list_stock]
-            df_stock = df_stock.filter(df_stock.Stock.isin(stk_list))
-            list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
-            list_period = df_stock.select("Period").rdd.flatMap(lambda x: x).collect()
-            list_interval = df_stock.select("Interval").rdd.flatMap(lambda x: x).collect()
-            print("Invalid stock names are removed. List of stocks of the portfolio: ")
-            print(stk_list)
-            num_shares_list = []
-            min_date = datetime.datetime(1000, 1, 1)
-            max_date = datetime.datetime.now()
-            for i in range(len(list_stock)):
-
-                num_shares = int(input("Please, input the number of shares of " + list_stock[i] + ": "))
-                num_shares_list.append(num_shares)
-
-                stock_df = read_stock_data(spark, list_stock[i], list_period[i], list_interval[i])
-
-                min_date_aux = stock_df.select("DateTime").rdd.min()[0]
-                max_date_aux = stock_df.select("DateTime").rdd.max()[0]
-
-                if min_date_aux > min_date:
-                    min_date = min_date_aux
-
-                if max_date_aux < max_date:
-                    max_date = max_date_aux
-
-            write_portfolio_list(spark, stk_list, num_shares_list, min_date, max_date)
-            read_portfolio_list(spark).show(truncate=False)
-
-        elif option == 5:
-            read_portfolio_list(spark).show(truncate=False)
-
-        elif option == 6:
             try:
                 read_stock_log(spark).orderBy("Stock").show(truncate=False)
                 stk = input("Please, input a valid stock name: ").upper()
@@ -135,19 +88,34 @@ def main():
                 interval = get_valid_interval()
                 df = read_stock_data(spark, stk, period, interval)
                 df.show(truncate=False)
-            except:
+            except (TypeError, AttributeError):
                 print("Empty data, try option -> 1. Download stock data.\n")
+
+        elif option == 4:
+            df_stock = read_stock_log(spark)
+            df_stock.orderBy("Stock").show(truncate=False)
+            list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
+            list_period = df_stock.select("Period").rdd.flatMap(lambda x: x).collect()
+            list_interval = df_stock.select("Interval").rdd.flatMap(lambda x: x).collect()
+            for i in range(len(list_stock)):
+                df = download_stock_data(spark, list_stock[i], list_period[i], list_interval[i])
+                write_stock_data(spark, df, list_stock[i], list_period[i], list_interval[i])
+
+        elif option == 5:
+            df_stock = read_stock_log(spark)
+            df_stock.orderBy("Stock").filter("Period = 'max'").orderBy("Stock").show(truncate=False)
+            list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
+            stk_list = input("Please, input a list separated by comma of stocks: ").upper().replace(" ", "").split(",")
+            create_portfolio(spark, df_stock, stk_list, list_stock)
+
+        elif option == 6:
+            read_portfolio_list(spark).show(truncate=False)
+
         elif option == 7:
             df_portfolio = read_portfolio_list(spark)
             df_portfolio.orderBy("ID").show(truncate=False)
-            portfolio = int(input("Please, input a portfolio ID: "))
-            stocks = df_portfolio.filter(df_portfolio.ID == portfolio) \
-                .select("Stock_List").rdd.flatMap(lambda x: x).collect()[0]
-            min_date = df_portfolio.filter(df_portfolio.ID == portfolio) \
-                .select("Min_Date").rdd.flatMap(lambda x: x).collect()[0]
-            max_date = df_portfolio.filter(df_portfolio.ID == portfolio) \
-                .select("Max_Date").rdd.flatMap(lambda x: x).collect()[0]
-            correlation_matrix_portfolio(spark, stocks, min_date, max_date).show()
+            id_portfolio = int(input("Please, input a portfolio ID: "))
+            correlation_matrix_portfolio(spark, df_portfolio, id_portfolio).show()
 
 
 if __name__ == "__main__":
--- project/finance/finance_utils.py
+++ project/finance/finance_utils.py
@@ -3,17 +3,25 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 import scipy.stats as stats
-from io_stockdata.io_stockdata import read_stock_data
+import datetime
+from io_stockdata.io_stockdata import read_stock_data, write_portfolio_list, read_portfolio_list
 
 
-def correlation_matrix_portfolio(spark, stock_list, min_date, max_date):
+def correlation_matrix_portfolio(spark, df_portfolio, id_portfolio):
     matrix = []
     stock_close_prices = {}
+
+    stock_list = df_portfolio.filter(df_portfolio.ID == id_portfolio) \
+        .select("Stock_List").rdd.flatMap(lambda x: x).collect()[0]
+    min_date = df_portfolio.filter(df_portfolio.ID == id_portfolio) \
+        .select("Min_Date").rdd.flatMap(lambda x: x).collect()[0]
+    max_date = df_portfolio.filter(df_portfolio.ID == id_portfolio) \
+        .select("Max_Date").rdd.flatMap(lambda x: x).collect()[0]
 
     for stock in stock_list:
         df_stock = read_stock_data(spark, stock, "max", "1d")
         df_stock = df_stock.filter(df_stock.DateTime > min_date) \
-                           .filter(df_stock.DateTime < max_date)
+            .filter(df_stock.DateTime < max_date)
 
         stock_close_prices[stock] = df_stock.select("Close").rdd.flatMap(lambda x: x).collect()
 
@@ -22,9 +30,40 @@ def correlation_matrix_portfolio(spark, stock_list, min_date, max_date):
         matrix.append([stock])
         for stock2 in stock_list:
             c, p = stats.pearsonr(stock_close_prices[stock], stock_close_prices[stock2])
-            matrix[-1].append(float(c))
+            matrix[-1].append(round(float(c), 5))
 
     stock_list.insert(0, "Correlations")
     df = spark.createDataFrame(matrix, stock_list)
 
     return df
+
+
+def create_portfolio(spark, df_stock, stk_list, list_stock):
+    stk_list = [i for i in stk_list if i in list_stock]
+    df_stock = df_stock.filter(df_stock.Stock.isin(stk_list))
+    list_stock = df_stock.select("Stock").rdd.flatMap(lambda x: x).collect()
+    list_period = df_stock.select("Period").rdd.flatMap(lambda x: x).collect()
+    list_interval = df_stock.select("Interval").rdd.flatMap(lambda x: x).collect()
+    print("Invalid stock names are removed. List of stocks of the portfolio: ")
+    print(stk_list)
+    num_shares_list = []
+    min_date = datetime.datetime(1000, 1, 1)
+    max_date = datetime.datetime.now()
+    for i in range(len(list_stock)):
+
+        num_shares = int(input("Please, input the number of shares of " + list_stock[i] + ": "))
+        num_shares_list.append(num_shares)
+
+        stock_df = read_stock_data(spark, list_stock[i], list_period[i], list_interval[i])
+
+        min_date_aux = stock_df.select("DateTime").rdd.min()[0]
+        max_date_aux = stock_df.select("DateTime").rdd.max()[0]
+
+        if min_date_aux > min_date:
+            min_date = min_date_aux
+
+        if max_date_aux < max_date:
+            max_date = max_date_aux
+
+    write_portfolio_list(spark, stk_list, num_shares_list, min_date, max_date)
+    read_portfolio_list(spark).show(truncate=False)
